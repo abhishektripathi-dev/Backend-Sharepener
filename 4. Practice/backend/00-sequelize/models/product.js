8d1542574ce34f90ecabb636ae06056df59e6486
abhishek8182 <abhishektripathi3196@gmail.com>
2024-12-28 17:12:09 +0530

    sequelize practice on 28 dec

--- 4. Practice/backend/00-sequelize/models/product.js
+++ 4. Practice/backend/00-sequelize/models/product.js
@@ -1,30 +1,30 @@
-const {DataTypes} = require('sequelize');
+const db = require('../util/database');
 
-const sequelize = require('../util/database');
+const Cart = require('./cart');
 
-const Product = sequelize.define('product', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  price: {
-    type: DataTypes.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  description: {
-    type: DataTypes.STRING,
-    allowNull: false
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
-});
 
-module.exports = Product;
+  save() {
+    return db.execute(
+      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
+  }
+
+  static deleteById(id) {}
+
+  static fetchAll() {
+    return db.execute('SELECT * FROM products');
+  }
+
+  static findById(id) {
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
+  }
+};
